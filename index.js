295810f743fb4c12cba1f0e5cef62d517ebbddfb
Sai Chakri D <98957092+SaiChakri-d@users.noreply.github.com>
2023-05-11 19:40:30 +0530

    cors updated again

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({ message: "welcome" });
+  res.json({ message: "Welcome! Csynergy API by Sai Chakri" });
 });
 
 app.use("/projects", project);
